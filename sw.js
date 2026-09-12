ee76fd7ad3f9e3a5909e7e4bdf0152584193779f
nestorfernando1 <nestoravila1@gmail.com>
2026-09-11 22:12:20 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const VERSION = 'ingles-v1';
+const ARCHIVOS = [
+    'index.html',
+    'manifest.webmanifest',
+    'icons/icon-192.png',
+    'icons/icon-512.png'
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(VERSION).then(function (cache) {
+            return cache.addAll(ARCHIVOS);
+        }).then(function () {
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (claves) {
+            return Promise.all(
+                claves.filter(function (c) {
+                    return c !== VERSION;
+                }).map(function (c) {
+                    return caches.delete(c);
+                })
+            );
+        }).then(function () {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (resp) {
+            return resp || fetch(e.request);
+        })
+    );
+});
